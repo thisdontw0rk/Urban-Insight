6350b06f66a934c7e953e75fa861759e862bbdf2
Kosiso Igbokwe <48086838+igbokwe-kosi@users.noreply.github.com>
2025-11-08 14:39:58 -0700

    Replace initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-5xl text-red-600">Urban Insight</h1>
+      <p className="text-base text-[#ff0066]">Using GIS</p>
+    </div>
+  );
+}
+
+export default App;
